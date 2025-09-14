33ec414037aa89c807587c7d02daf17bfb0a152e
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-09-14 12:13:45 -0300

    criado componente e pagina de login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +10,32 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    })
+  }
+  
+  onSubmit(): void {
+    if(this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: () => {
+          console.log('logado')
+          // this.router.navigate(['/dashboard'])
+        },
+        error: (err) => {
+          console.error('Erro ao realizar login: ', err);
+        }
+      })
+    }
+  }
 }
