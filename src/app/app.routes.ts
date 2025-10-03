3e9c5ea8d683d66163a8586629e56e3e08e36507
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-10-03 15:28:51 -0300

    feat: implementa rota e guarda da pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { MainComponent } from './core/components/main/main.component';
+import { HomeComponent } from './features/home/home.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    {
+        path: 'app',
+        component: MainComponent,
+        canActivate: [authGuard],
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent }
+        ],
+
+    },
 ];
